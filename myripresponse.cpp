3fbc02dc9194cf00fc2d72f2488ff12fa56dcef8
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-09-28 08:18:00 +0200

    parameters

--- myripresponse.cpp
+++ myripresponse.cpp
@@ -5,13 +5,13 @@
 #include <climits>
 #include <ctype.h>
 
-char * ifce = NULL;
+const char * ifce = NULL;
 int addr[4] = {-1,};
 int mask = -1;
 int nexthop[4] = {-1,};
 int metric = -1;
 int rttag = -1;
-char * passwd = NULL;
+const char * passwd = NULL;
 
 void printUsageAndExit() {
     std::cerr << "Usage: ./myripresponse {-i <interface>} -r <IPv4>/[8-30] {-n <IPv4>} {-m [0-16]} {-t [0-65535]} {-p <password>}\n";
@@ -28,7 +28,7 @@ int parseInt(   const char * s,
     for(unsigned i = 0; i < strlen(s); i++) {
         if( isdigit(s[i]) ) {
             result += s[i] - '0';
-            if( inRange(result) ) printUsageAndExit();
+            if( !inRange(result) ) printUsageAndExit();
             result *= 10;
             len = i+1;
         }
@@ -46,24 +46,37 @@ int parseInt(   const char * s,
 
 int main(int argc, char *argv[]) {
     if(argc%2 != 0 && argc >= 3) {
+        std::function<bool(int)> inChar = [](int x){ return x >= 0 && x < 256; };
         for(int i = 1; i < argc; i+=2) {
             // -i
             if( !strcmp(argv[i],"-i") ) {
                 if(ifce != NULL) printUsageAndExit();
                 else ifce = argv[i+1];
             }
+            // -r
             else if( !strcmp(argv[i],"-r") ) {
                 if(addr[0] != -1) printUsageAndExit();
                 if(sscanf(argv[i+1], "%d.%d.%d.%d/%d", &addr[0], &addr[1], &addr[2], &addr[3], &mask) != 5) printUsageAndExit();
+                if( !inChar(addr[0]) || !inChar(addr[1]) || !inChar(addr[2]) || !inChar(addr[3]) || mask < 8 || mask > 30 ) printUsageAndExit();
+                char pom[19];
+                sprintf(pom, "%d.%d.%d.%d/%d", addr[0], addr[1], addr[2], addr[3], mask);
+                if( strcmp(pom,argv[i+1]) ) printUsageAndExit();
             }
+            // -n
             else if( !strcmp(argv[i],"-n") ) {
                 if(nexthop[0] != -1) printUsageAndExit();
                 if(sscanf(argv[i+1], "%d.%d.%d.%d", &nexthop[0], &nexthop[1], &nexthop[2], &nexthop[3]) != 4) printUsageAndExit();
+                if( !inChar(nexthop[0]) || !inChar(nexthop[1]) || !inChar(nexthop[2]) || !inChar(nexthop[3]) ) printUsageAndExit();
+                char pom[16];
+                sprintf(pom, "%d.%d.%d.%d", nexthop[0], nexthop[1], nexthop[2], nexthop[3]);
+                if( strcmp(pom,argv[i+1]) ) printUsageAndExit();
             }
+            // -m
             else if( !strcmp(argv[i],"-m") ) {
                 if(metric != -1) printUsageAndExit();
                 else metric = parseInt(argv[i+1], [](int x){return (x>=0)&&(x<=16);} );
             }
+            // -t
             else if( !strcmp(argv[i],"-t") ) {
                 if(rttag != -1) printUsageAndExit();
                 else rttag = parseInt(argv[i+1], [](int x){return (x>=0)&&(x<=65535);} );
@@ -76,10 +89,24 @@ int main(int argc, char *argv[]) {
             // other
             else printUsageAndExit();
         }
+
+        // set default
+        if(addr[0] == -1) printUsageAndExit();
+        if(metric == -1) metric = 1;
+        if(nexthop[0] == -1) nexthop[0] = nexthop[1] = nexthop[2] = nexthop[3] = 0;
+        if(rttag == -1) rttag = 0;
+        if(passwd == NULL) passwd = "";
+        if(ifce == NULL) ifce = ""; // ???
     }
     else printUsageAndExit();
 
 
+    std::cout << "Interface: " << ifce << "\n";
+    std::cout << "Address: " << addr[0] << "." << addr[1] << "." << addr[2] << "." << addr[3] << "/" << mask << "\n";
+    std::cout << "Next hop: " << nexthop[0] << "." << nexthop[1] << "." << nexthop[2] << "." << nexthop[3] << "\n";
+    std::cout << "Metric: " << metric << "\n";
+    std::cout << "Route Tag: " << rttag << "\n";
+    std::cout << "Password: " << passwd << "\n";
 
 
 }
