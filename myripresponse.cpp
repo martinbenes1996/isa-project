e90aec803f110f9d063cc7d055c3a01c75debc11
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-09-28 17:29:24 +0200

    docs

--- myripresponse.cpp
+++ myripresponse.cpp
@@ -5,59 +5,96 @@
 #include <climits>
 #include <ctype.h>
 
-const char * ifce = NULL;
-int addr[4] = {-1,};
-int mask = -1;
-int nexthop[4] = {-1,};
-int metric = -1;
-int rttag = -1;
-const char * passwd = NULL;
+/* ------------------------------------------------------------- */
+const char * ifce = NULL;   /**< Interface name.*/
+int addr[4] = {-1,};        /**< Address of the fake network. */
+int mask = -1;              /**< Mask of the fake network. */
+int nexthop[4] = {-1,};     /**< Address of fake route next-hop. */
+int metric = -1;            /**< Hop-count. */
+int rttag = -1;             /**< Route tag. */
+const char * passwd = NULL; /**< Password. */
+/* ------------------------------------------------------------- */
 
+/**
+ * @brief Prints the usage and then terminate the program.
+ */
 void printUsageAndExit() {
     std::cerr << "Usage: ./myripresponse {-i <interface>} -r <IPv4>/[8-30] {-n <IPv4>} {-m [0-16]} {-t [0-65535]} {-p <password>}\n";
     exit(1);
 }
 
+
+/**
+ * @brief   Parses string into integer.
+ * @param s         String including integer.
+ * @param isRange   Function saying if input integer is in range of possible values.
+ * @param isEnd     Function saying if input character is end of string.
+ * @param length    If not NULL, function writes count of digits.
+ * @returns Parsed integer.
+ */
 int parseInt(   const char * s, 
                 std::function<bool(int)> inRange = [](int){ return true; },
                 std::function<bool(char c)> isEnd = [](char c){ return isspace(c); },
                 int* length = NULL) {
+    
+    long result = 0; // Return value
+    int len = 0;     // Digit count
 
-    long result = 0;
-    int len = 0;
-    for(unsigned i = 0; i < strlen(s); i++) {
+    for(unsigned i = 0; i < strlen(s); i++) { // go around string
+        // digit
         if( isdigit(s[i]) ) {
             result += s[i] - '0';
             if( !inRange(result) ) printUsageAndExit();
             result *= 10;
             len = i+1;
         }
+        // ending character
         else if( isEnd(s[i]) ) { 
             len = i;
             break;
         }
+        // other - fail
         else printUsageAndExit();
     }
     result /= 10;
+    
+    // out of range || no number
     if( !inRange(result) || len == 0) printUsageAndExit();
+    // return
     if( length != NULL ) *length = len;
     return (int)result;
 }
 
+/**
+ * @brief   Main function.
+ * @param argc          Count of arguments.
+ * @param argv          Arguments.
+ * @returns Exit code. 
+ */
 int main(int argc, char *argv[]) {
+    // correct count
     if(argc%2 != 0 && argc >= 3) {
-        std::function<bool(int)> inChar = [](int x){ return x >= 0 && x < 256; };
-        for(int i = 1; i < argc; i+=2) {
+        std::function<bool(int)> inChar = [](int x){ return x >= 0 && x < 256; }; // function (i in <0;255>)
+
+        for(int i = 1; i < argc; i+=2) { // go around argument couples
+
             // -i
             if( !strcmp(argv[i],"-i") ) {
                 if(ifce != NULL) printUsageAndExit();
                 else ifce = argv[i+1];
             }
+            // -p
+            else if( !strcmp(argv[i],"-p") ) {
+                if(passwd != NULL) printUsageAndExit();
+                else passwd = argv[i+1];
+            }
+        
             // -r
             else if( !strcmp(argv[i],"-r") ) {
                 if(addr[0] != -1) printUsageAndExit();
                 if(sscanf(argv[i+1], "%d.%d.%d.%d/%d", &addr[0], &addr[1], &addr[2], &addr[3], &mask) != 5) printUsageAndExit();
                 if( !inChar(addr[0]) || !inChar(addr[1]) || !inChar(addr[2]) || !inChar(addr[3]) || mask < 8 || mask > 30 ) printUsageAndExit();
+                // prevents appendix
                 char pom[19];
                 sprintf(pom, "%d.%d.%d.%d/%d", addr[0], addr[1], addr[2], addr[3], mask);
                 if( strcmp(pom,argv[i+1]) ) printUsageAndExit();
@@ -67,10 +104,12 @@ int main(int argc, char *argv[]) {
                 if(nexthop[0] != -1) printUsageAndExit();
                 if(sscanf(argv[i+1], "%d.%d.%d.%d", &nexthop[0], &nexthop[1], &nexthop[2], &nexthop[3]) != 4) printUsageAndExit();
                 if( !inChar(nexthop[0]) || !inChar(nexthop[1]) || !inChar(nexthop[2]) || !inChar(nexthop[3]) ) printUsageAndExit();
+                // prevents appendix
                 char pom[16];
                 sprintf(pom, "%d.%d.%d.%d", nexthop[0], nexthop[1], nexthop[2], nexthop[3]);
                 if( strcmp(pom,argv[i+1]) ) printUsageAndExit();
             }
+
             // -m
             else if( !strcmp(argv[i],"-m") ) {
                 if(metric != -1) printUsageAndExit();
@@ -81,11 +120,7 @@ int main(int argc, char *argv[]) {
                 if(rttag != -1) printUsageAndExit();
                 else rttag = parseInt(argv[i+1], [](int x){return (x>=0)&&(x<=65535);} );
             }
-            // -p
-            else if( !strcmp(argv[i],"-p") ) {
-                if(passwd != NULL) printUsageAndExit();
-                else passwd = argv[i+1];
-            }
+            
             // other
             else printUsageAndExit();
         }
@@ -98,6 +133,8 @@ int main(int argc, char *argv[]) {
         if(passwd == NULL) passwd = "";
         if(ifce == NULL) ifce = ""; // ???
     }
+    
+    // incorrect count - fail
     else printUsageAndExit();
 
 
--- myripsniffer.cpp
+++ myripsniffer.cpp
@@ -4,52 +4,81 @@
 #include <string>
 #include <pcap/pcap.h>
 
-char * ifce;
+/* ------------------------------ */
+char * ifce; /**< Interface name. */
+/* ------------------------------ */
 
+/**
+ * @brief Prints error and exits.
+ * @param error         PCAP error code.
+ * @param errcode       Error code.
+ */
 void printErrorAndExit(int error, int errcode) {
     std::cerr << pcap_statustostr(error) << "\n";
     exit(errcode);
 }
+/**
+ * @brief Prints error and exits.
+ * @param error         String to print.
+ * @param errcode       Error code.
+ */
 void printErrorAndExit(std::string error, int errcode) {
     std::cerr << error << "\n";
     exit(errcode);
 }
 
+/**
+ * @brief Cover class for PCAP library.
+ */
 class Device {
     public:
+        /**
+         * @brief Constructor.
+         * @param ifce      Interface.
+         */
         Device(std::string ifce) {
             char errbuf[PCAP_ERRBUF_SIZE];
             int status;
 
-            handle = pcap_create(ifce.c_str(), errbuf);
+            handle = pcap_create(ifce.c_str(), errbuf); // create handle
             if(handle == NULL) printErrorAndExit(errbuf, 2);
 
-            status = pcap_set_promisc(handle, 1);
+            status = pcap_set_promisc(handle, 1); // set promiscuous mode
             if(status) printErrorAndExit(status, 3);
 
-            status = pcap_activate(handle);
+            status = pcap_activate(handle); // activate handle
             if(status) printErrorAndExit(status, 4);
         }
 
-        ~Device() {
-            pcap_close(handle);
-        }
+        /**
+         * @brief Destructor.
+         */
+        ~Device() { pcap_close(handle); }
+
     private:
-        pcap_t *handle;
+        pcap_t *handle; /**< Handle. */
 };
 
-
+/**
+ * @brief   Main function.
+ * @param argc          Count of arguments.
+ * @param argv          Arguments.
+ * @returns Exit code. 
+ */
 int main(int argc, char *argv[]) {
+    
+    // -i
     if(argc == 3 && !strcmp(argv[1], "-i") ) {
         ifce = argv[2];
     } 
+    // other - fail
     else printErrorAndExit("Usage: ./myripsniffer -i <interface>\n", 1);
 
+    // connect to device
     Device d(ifce);
     
-    
-
-
+    // Listen
+    // ...
 
     return 0;
 }
